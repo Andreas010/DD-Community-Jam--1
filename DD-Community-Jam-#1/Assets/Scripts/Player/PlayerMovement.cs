297608c4236ec119777aa6446d11a6c3c3cd85e8
TyJohnston <68244896+TyJohnston@users.noreply.github.com>
2021-06-10 10:11:45 -0400

    Player Movement!
    Player movement, You can now move around the level! :D

---
+++ DD-Community-Jam-#1/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [Header("References")]
+    Rigidbody2D rig;
+    [SerializeField] LayerMask groundLayer;
+
+    [Header("Parameters")]
+    [SerializeField] Vector2 speeds; //speeds.x = normal speed speeds.y = running speed
+    [SerializeField] float jumpForce = 16;
+    float jumpTimeCounter;
+    [SerializeField] float jumpTime = 0.2f;
+    [SerializeField] float fallMultiplier = 2.5f;
+    [SerializeField] float downwardJumpForce = -3;
+    [SerializeField] float minDistFromGround = 0.6f;
+
+    //bools
+    bool sprinting;
+    private bool isJumping;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        #region
+        bool IsGrounded()
+        {
+            Vector2 position = transform.position;
+            Vector2 direction = Vector2.down;
+            float distance = minDistFromGround;
+
+            RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+            if (hit.collider != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        bool IsTouchingCeiling()
+        {
+            Vector2 position = transform.position;
+            Vector2 direction = Vector2.up;
+            float distance = minDistFromGround;
+
+            RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+            if (hit.collider != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+        ////////////////////////////////////////////////
+        #region
+        sprinting = Input.GetButton("Sprint");
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //horizontal Movement
+        if (!sprinting) rig.velocity = new Vector2(x * speeds.x, rig.velocity.y); //speeds.x = normal speed
+        else rig.velocity = new Vector2(x * speeds.y, rig.velocity.y); //speeds.y = sprinting speed
+
+        //jump
+        if (IsGrounded() && (Input.GetButton("Jump")))
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rig.velocity = new Vector2(rig.velocity.x, jumpForce);
+        }
+
+        if ((Input.GetButton("Jump")) && isJumping == true)
+        {
+            if (!IsTouchingCeiling())
+            {
+                float t_jumpforce = jumpForce;
+
+                if (jumpTimeCounter > 0)
+                {
+                    rig.velocity = new Vector2(rig.velocity.x, t_jumpforce);
+                    jumpTimeCounter -= Time.deltaTime;
+                    t_jumpforce -= Time.deltaTime;
+                }
+                else { isJumping = false; }
+            }
+            else { isJumping = false; if (rig.velocity.y > 0) { rig.velocity = new Vector2(rig.velocity.x, -downwardJumpForce); } }
+
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+            //rig.AddForce(Vector2.up * -5, ForceMode2D.Impulse);
+            if (rig.velocity.y > 0)
+                rig.velocity = new Vector2(rig.velocity.x, -downwardJumpForce);
+        }
+
+        if (rig.velocity.y < 0)
+        {
+            rig.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rig.velocity.y > 0 && !(Input.GetButton("Jump")))
+        { rig.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime; }
+        #endregion
+    }
+}
