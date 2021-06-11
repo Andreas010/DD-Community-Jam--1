b8b35e933e1898a7d161594cf4799135f5b80060
TyJohnston <68244896+TyJohnston@users.noreply.github.com>
2021-06-11 09:30:49 -0400

    CameraMovement
    Camera lerps

---
+++ DD-Community-Jam-#1/Assets/Scripts/Player/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    [SerializeField] Transform player;
+
+    [SerializeField] float lerpTime;
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, player.position.y, -10), lerpTime);
+    }
+}
