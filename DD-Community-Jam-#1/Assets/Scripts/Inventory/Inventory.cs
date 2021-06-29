683464bca314819f12d60fa0dac3616769e0cc58
SomeRandomDude010 <piotr@kutylowski.de>
2021-06-29 21:56:37 +0200

    Inventory System Progress
    -Changable Building Block in Inventory
    -Function to remove a count of a certain Item
    -System incompatible with weapons because it needs scriptable objects

--- DD-Community-Jam-#1/Assets/Scripts/Inventory/Inventory.cs
+++ DD-Community-Jam-#1/Assets/Scripts/Inventory/Inventory.cs
@@ -28,18 +28,29 @@ public class Inventory : MonoBehaviour
     public Item test1;
     public Item test2;
 
+    InventoryItem selected;
+    InventoryItem currItem;
+    TerrainEditor editor;
+    Item defaultItem;
+
     bool isInventory = false;
+    bool first = true;
+
+    float defRange;
 
     void Start()
     {
         items = new InventoryItem[slots.Length];
+        editor = FindObjectOfType<TerrainEditor>();
+        selected = null;
+        defRange = editor.range;
 
         transform.GetChild(0).gameObject.SetActive(false);
         info.SetActive(false);
 
         defColor = slotImg[0].color;
 
-        Item defaultItem = ScriptableObject.CreateInstance("Item") as Item;
+        defaultItem = ScriptableObject.CreateInstance("Item") as Item;
         defaultItem.maxCount = 0;
         defaultItem.type = Item.ItemType.None;
         defaultItem.scriptObject = null;
@@ -89,14 +100,6 @@ public class Inventory : MonoBehaviour
 
     public void AddItem(Item item, int count)
     {
-        //TODO: Add Item (Dramatic Music)
-
-        //Find Prefered Slot
-        //Check If Item Can Be Added
-        //Add Item (Finally)
-        //Update HUD
-        //Hope It Is Working
-
         TerrainType terrainType = null;
         Item.ItemType type = item.type;
         int maxCount = item.maxCount;
@@ -108,6 +111,8 @@ public class Inventory : MonoBehaviour
             default:
                 return;
         }
+
+        //Spagetti-Code
 
         int index = 0xDEAD;
         int voidIndex = 0xDEAD;
@@ -139,6 +144,19 @@ public class Inventory : MonoBehaviour
             index = voidIndex;
 
         items[index] = new InventoryItem(item, count + items[index].count, index);
+
+        if (first)
+        {
+            switch (items[index].item.type)
+            {
+                case Item.ItemType.Block:
+                    SelectAsBlock(items[index]);
+                    break;
+                default:
+                    break;
+            }
+            first = false;
+        }
     }
 
     public void Clicked(int index)
@@ -148,11 +166,64 @@ public class Inventory : MonoBehaviour
         if (item.item.type == Item.ItemType.None)
             return;
 
+        currItem = item;
+
         info.transform.position = new Vector3(slots[index].transform.position.x - 90, slots[index].transform.position.y - 90, 0);
         info.SetActive(true);
 
         info.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = item.item.displayName;
 
         info.transform.GetChild(1).gameObject.GetComponent<TMP_Text>().text = $"Count: {item.count}";
+
+        info.transform.GetChild(2).gameObject.GetComponent<Button>().interactable = true;
+
+        if (selected.slotID == item.slotID)
+        {
+            info.transform.GetChild(2).GetChild(0).gameObject.GetComponent<TMP_Text>().text = "Already Selected";
+            info.transform.GetChild(2).gameObject.GetComponent<Button>().interactable = false;
+        }
+
+        if(item.item.type == Item.ItemType.Block)
+        {
+            info.transform.GetChild(2).GetChild(0).gameObject.GetComponent<TMP_Text>().text = "Select As Building Block";
+        }
+    }
+
+    void SelectAsBlock(InventoryItem item)
+    {
+        editor.placeTile = item.item.scriptObject as TerrainType;
+        selected = item;
+    }
+
+    public void ClickedToSelect()
+    {
+        editor.range = defRange;
+
+        switch (currItem.item.type)
+        {
+            case Item.ItemType.Block:
+                SelectAsBlock(currItem);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void RemoveItem(int slotID, int count)
+    {
+        InventoryItem item = items[slotID];
+
+        if (item.count - count <= 0)
+        {
+            if(slotID == selected.slotID)
+            {
+                selected = null;
+                editor.range = 0;
+            }
+
+            item = new InventoryItem(defaultItem, 0, slotID);
+        }
+        else
+            item.count -= count;
     }
 }
