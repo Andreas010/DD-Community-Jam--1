de01515901f42a4b9a1f80cec00b658a6073e5a5
TortMcGee <88456679+TortMcGee@users.noreply.github.com>
2021-08-04 15:24:57 -0400

    made camera smoother

--- DD-Community-Jam-#1/Assets/Scripts/Player/CameraScript.cs
+++ DD-Community-Jam-#1/Assets/Scripts/Player/CameraScript.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-
     public Transform player;
     Vector3 originalPos;
 
     public float lerpTime;
+    public Vector3 offset;
+    public float smoothing;
 
-    void LateUpdate()
+    void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, player.position.y, -10), lerpTime);
+        //transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, player.position.y, -10), lerpTime);
+        if (player != null)
+        {
+            //sets position to players with smoothing
+            Vector3 desiredPosition = player.transform.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothing);
+            transform.position = smoothedPosition;
+        }
     }
 
     public void CameraShake(float frequency)
-    {
-        
+    {  
         for (int i = 0; i < 30; i++)
         {
             originalPos = player.position;
