ed8017657cb99fd1830a5a7cda530ca5669862f7
Andreas010 <winjas@gmx.net>
2021-08-05 08:35:54 +0200

    Probably Last changes for the jam version by me

---
+++ DD-Community-Jam-#1/Assets/DamagePlayerOnCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayerOnCollision : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerHealth>().ModifyHealth(-1);
+            collision.GetComponent<PlayerHealth>().MakeInvincible(2);
+
+        }
+    }
+}
