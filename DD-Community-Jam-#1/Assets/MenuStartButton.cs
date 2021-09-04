845c7090c9ab63e2eb2f6245ea4bb78b382a393f
Andreas010 <winjas@gmx.net>
2021-09-04 20:37:31 +0200

    Yeesssssss

---
+++ DD-Community-Jam-#1/Assets/MenuStartButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuStartButton : MonoBehaviour
+{
+    public void Yes()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
